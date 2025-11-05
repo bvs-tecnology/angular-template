10b21b18407fc92e7f1e6e96754b75e691e5301d
Bruno Silva <brunovinicius0704@gmail.com>
2025-11-04 23:24:12 -0300

    feat: ajuste de urls

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,9 +1,11 @@
 export const environment = {
 	production: false,
+	angularUrl: 'https://dev.bvsilva.com',
+	apiUrl: 'https://bvs-api.bvsilva.com',
 	keycloak: {
 		url: 'https://auth.bvsilva.com',
 		realm: 'development',
 		clientId: 'template',
 	},
-	pushPublicKey: '',
+	pushPublicKey: 'BESkM2nAGlVkbo04Hn-Y6s2LhNWxMPn_3wdLscRVZmzn3ZWgNuzZQl4UpLYeQFQM0krZWhKqv_qoRbkyLryIEjs',
 };
